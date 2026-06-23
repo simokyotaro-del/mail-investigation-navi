7f4e9636f99419a535fb63db93cf508c3ea985c0
simokyotaro-del <simo.kyotaro@gmail.com>
2026-06-23 14:10:48 +0900

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,17 +1,18 @@
-/* 画面切替 */
 function showScreen(id){
   document.querySelectorAll('.screen').forEach(s=>{
     s.classList.remove('active');
   });
+
   document.getElementById(id).classList.add('active');
 }
 
-/* フロー */
 function nextFlow(){
-  const file = document.getElementById("imageInput").files[0];
 
-  if (!file){
-    alert("画像を選択してください");
+  const file =
+    document.getElementById("imageInput").files[0];
+
+  if(!file){
+    alert("スクリーンショットを選択してください");
     return;
   }
 
