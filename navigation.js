dbbf7cbb41434c90531ff6accd76a2e57c47242e
simokyotaro-del <simo.kyotaro@gmail.com>
2026-07-02 15:20:25 +0900

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,2 +1,15 @@
-let suspiciousWords =
-  detectDangerWords(text);
+const mailApp = detectMailApp(text);
+
+const guide = createMailGuide(mailApp);
+
+const suspiciousWords = detectDangerWords(text);
+
+document.getElementById("appResult").innerText = guide;
+
+if(suspiciousWords.length > 0){
+
+  document.getElementById("appResult").innerText +=
+    "\n\n⚠ 注意ワード検出\n" +
+    suspiciousWords.join("、");
+
+}
